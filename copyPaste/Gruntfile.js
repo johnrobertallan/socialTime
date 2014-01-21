d71fec16b6fc1f28c0c3fdec6ac3c5d1266b1956
John Robert Allan <john>
2014-01-20 16:20:06 -0800

    cleaned up build

--- copyPaste/Gruntfile.js
+++ copyPaste/Gruntfile.js
@@ -3,30 +3,16 @@ module.exports = function(grunt) {
     //Project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                mangle: true,
-                compress: true,
-                except: ['jQuery']
-            },
-            my_target: {
-                files: {
-                    '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
-                }
-            }
-        },
         bookmarklet: {
             generate: {
                 body: '<%= pkg.name %>.min.js',
-                out: '../<%= pkg.name %>.bookmarklet.js'
+                out: '<%= pkg.name %>.bookmarklet.js'
             }
         }
     });
     
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bookmarklet-thingy');
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify', 'bookmarklet']);
+    grunt.registerTask('default', ['bookmarklet']);
 };
